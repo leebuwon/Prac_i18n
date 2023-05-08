66e6512a133e7feba2962ff99ef1d3722250d5ec
bw1611 <bw1611@naver.com>
2023-05-08 09:43:50 +0900

    init commit

---
+++ src/main/java/com/ll/pracI18n/PracI18nApplication.java
@@ -0,0 +1,13 @@
+package com.ll.pracI18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracI18nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracI18nApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/pracI18n/PracI18nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.pracI18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracI18nApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
